18f9b43b2ffccd0a2f178929575b727d645fad9a
Angel Aviles <elangelcotaymas@gmail.com>
2023-04-19 19:08:06 -0700

    Algoritmo Bresenham

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function setup() {
 function draw() {
   background(220);
 
-  ddarect(50,50,20,20);
+  ddarect(50, 50, 20, 20);
 
   if (colision(mouseX - 10, mouseY - 10, 20, 20, 50, 50, 20, 20)) {
     colord = '#FF0000'
@@ -19,50 +19,42 @@ function draw() {
 
 function ddarect(x1, y1, width, height) {
   // verticales
-  ddaLine(x1,y1,x1+width,y1);
-  ddaLine(x1,y1+height,x1+width,y1+height);
+  bresenhamLine(x1, y1, x1 + width, y1);
+  bresenhamLine(x1, y1 + height, x1 + width, y1 + height);
   //horizontales
-  ddaLine(x1,y1,x1,y1+height);
-  ddaLine(x1+width,y1,x1+width,y1+height);
+  bresenhamLine(x1, y1, x1, y1 + height);
+  bresenhamLine(x1 + width, y1, x1 + width, y1 + height);
 }
 
-function ddaLine(x1, y1, x2, y2) {
-  // Validar si la línea es vertical
-  if (x1 === x2) {
-    if (y1 > y2) {
-      [y1, y2] = [y2, y1];
-    }
-    
-    for (let y = y1; y <= y2; y++) {
-      point(x1, y);
-    }
-    
-    return;
-  }
-  
-  let dx = x2 - x1;
-  let dy = y2 - y1;
-  let m = dy / dx;
-  
+function bresenhamLine(x1, y1, x2, y2) {
+
+  console.log('line '+x1+','+y1+' y '+x1+','+y1)
+
+  let dx = abs(x2 - x1);
+  let dy = -abs(y2 - y1);
+  let sx = x1 < x2 ? 1 : -1;
+  let sy = y1 < y2 ? 1 : -1;
+  let err = dx + dy;
   stroke(colord);
   strokeWeight(2);
-  
   point(x1, y1);
-  
-  let x = x1;
-  let y = y1;
-  while (x < x2) {
-    x = x + 1;
-    y = y + m;
-    point(x, y);
+  while (x1 != x2 || y1 != y2) {
+    let e2 = 2 * err;
+    if (e2 >= dy) {
+      err += dy;
+      x1 += sx;
+    }
+    if (e2 <= dx) {
+      err += dx;
+      y1 += sy;
+    }
+    point(x1, y1);
   }
-  
-  point(x2, y2);
 }
 
 function colision(x1, y1, w1, h1, x2, y2, w2, h2) {
   return x1 < x2 + w2 &&
-         x1 + w1 > x2 &&
-         y1 < y2 + h2 &&
-         y1 + h1 > y2;
+    x1 + w1 > x2 &&
+    y1 < y2 + h2 &&
+    y1 + h1 > y2;
 }
